356f4000a71d379817ecac77bbb814c774067b57
urizennnn <igamerryt@gmail.com>
2025-04-15 23:31:28 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
